af460b2f9642b9c8f6888d1720e2c5343a638d64
JOsunaL <juanosulor@gmail.com>
2018-02-21 10:41:01 +0100

    Tic Tac Toe

--- server/index.js
+++ server/index.js
@@ -161,6 +161,7 @@ io.on('connection', function (socket) {
     io.emit('ganador anterior', usuarios_en_juego[posicionU]);
     if (puntUsuarios[posicionU] === 3) {
       gana = usuarios_en_juego[posicionU];
+      usuarios_en_juego = [];
       io.emit('ganador', usuarios_en_juego[posicionU])
     }
   }
