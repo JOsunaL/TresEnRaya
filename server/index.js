f56b70a7d53ab59799c9658a751a154808f8519f
JOsunaL <juanosulor@gmail.com>
2018-02-21 10:58:31 +0100

    Tic Tac Toe

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ usuarios_en_juego = [];
 tablero = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 puntUsuarios = [0, 0];
 turno = 0;
-gana = "";
 conjMensajeP = [];
 conjMensajeO = [];
 
@@ -162,7 +161,6 @@ io.on('connection', function (socket) {
     io.emit('cambiar tablero', tablero);
     io.emit('ganador anterior', usuarios_en_juego[posicionU]);
     if (puntUsuarios[posicionU] === 3) {
-      gana = usuarios_en_juego[posicionU];
       io.emit('ganador', usuarios_en_juego[posicionU]);
       usuarios_en_juego = [];
       puntUsuarios = [0, 0];
